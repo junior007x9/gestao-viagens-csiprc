25e89d848fd45eff0b17976038dc341d3b2ee590
junior007x9 <santos.junior12@hotmail.com>
2026-03-25 08:51:05 -0300

    feat: setup completo do PWA

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+// @ts-ignore
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development", // Desativa no PC para não atrapalhar, funciona só na Vercel
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* suas outras configurações do next podem ficar aqui dentro */
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
